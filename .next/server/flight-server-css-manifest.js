233a588ebcd4c840cb5334fefcedfbe4779fc6f1
Sambuddy007 <ramtekesamyak007@gmail.com>
2024-08-16 21:10:08 +0530

    Update

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,14 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "C:\\Users\\ramte\\OneDrive\\Desktop\\Cust-Support-ChatBot\\src\\app\\layout.tsx": [
+      "C:\\Users\\ramte\\OneDrive\\Desktop\\Cust-Support-ChatBot\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "C:\\Users\\ramte\\OneDrive\\Desktop\\Cust-Support-ChatBot\\src\\app\\globals.css"
+    ]
+  },
+  "cssModules": {
+    "C:\\Users\\ramte\\OneDrive\\Desktop\\Cust-Support-ChatBot\\src\\app\\page": [
+      "C:\\Users\\ramte\\OneDrive\\Desktop\\Cust-Support-ChatBot\\src\\app\\globals.css",
+      "C:\\Users\\ramte\\OneDrive\\Desktop\\Cust-Support-ChatBot\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ]
+  }
+}
